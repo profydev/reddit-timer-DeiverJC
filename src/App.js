3ab886b992106b025e47bd148c88a92df62200f0
DeiverJC <deiver-jc@live.com>
2022-04-04 22:56:22 -0500

    Install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      App Placeholder
-      <input />
-      <input type="button" value="submit" />
+      <h1>App Placeholder</h1>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/search">Search</Link>
+      </nav>
+      <Outlet />
     </div>
   );
 }
